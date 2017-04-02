47a5cf70c1345160e5591badeaec48f3c4f92374
emilianogonzalezar <emilianogonzalez.ar@gmail.com>
2017-04-02 16:14:18 -0300

    Added refresh button.

--- app/src/main/java/com/egonzalez/simpleredditclient/view/MainActivity.java
+++ app/src/main/java/com/egonzalez/simpleredditclient/view/MainActivity.java
@@ -2,6 +2,9 @@ package com.egonzalez.simpleredditclient.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import com.egonzalez.simpleredditclient.R;
 import com.egonzalez.simpleredditclient.model.TopListing;
 import com.egonzalez.simpleredditclient.service.ServiceFactory;
@@ -17,10 +20,18 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean mMustRequestData = true;
 
+    private ImageButton mRefreshButton;
+
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRefreshButton = (ImageButton) findViewById(R.id.toolbar_refresh_button);
+        mRefreshButton.setOnClickListener(v -> {
+            mRefreshButton.setVisibility(View.GONE);
+            requestData();
+        });
 
         if (savedInstanceState != null) {
             mMustRequestData = savedInstanceState.getBoolean(SAVEDINSTANCESTATE_REQUESTDATA);
@@ -44,21 +55,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void requestData() {
+        mRefreshButton.setVisibility(View.GONE);
         showProgress();
         ServiceFactory.getInstance().getRedditService().getTopListing(0, 50).enqueue(new Callback<TopListing>() {
             @Override
             public void onResponse(final Call<TopListing> call, final Response<TopListing> response) {
+                mRefreshButton.setVisibility(View.VISIBLE);
                 showViewPager(response.body());
             }
 
             @Override
             public void onFailure(final Call<TopListing> call, final Throwable t) {
+                mRefreshButton.setVisibility(View.VISIBLE);
                 showConnectionError();
             }
         });
     }
 
     private void showConnectionError() {
+        mRefreshButton.setEnabled(false);
         final ConnectionProblemFragment connectionProblemFragment = new ConnectionProblemFragment();
 
         connectionProblemFragment.getRetryButtonClicked()
