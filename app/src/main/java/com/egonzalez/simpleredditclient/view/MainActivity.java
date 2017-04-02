5c1969a454f232ff346d58b8c6b98073eeb329bf
emilianogonzalezar <emilianogonzalez.ar@gmail.com>
2017-04-02 17:59:32 -0300

    * Removed unused commits.

--- app/src/main/java/com/egonzalez/simpleredditclient/view/MainActivity.java
+++ app/src/main/java/com/egonzalez/simpleredditclient/view/MainActivity.java
@@ -3,7 +3,6 @@ package com.egonzalez.simpleredditclient.view;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import com.egonzalez.simpleredditclient.R;
 import com.egonzalez.simpleredditclient.model.TopListing;
